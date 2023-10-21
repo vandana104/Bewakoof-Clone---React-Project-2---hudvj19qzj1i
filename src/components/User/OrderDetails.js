ddcdb7f6a6baa375e870a9b8fcb16ebeb049c8f1
vandana104 <111223104+vandana104@users.noreply.github.com>
2023-10-22 01:25:00 +0530

    Create OrderDetails.js

---
+++ src/components/User/OrderDetails.js
@@ -0,0 +1,100 @@
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useStateProvider } from "../../utils/StateProvider";
+import { useNavigate } from "react-router-dom";
+
+function OrderDetails() {
+  const [, dispatch] = useStateProvider();
+  const [bought, setBought] = useState([]);
+  useEffect(() => {
+    const boughts = JSON.parse(localStorage.getItem("bought"));
+    setBought(boughts);
+  }, []);
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      mt="30px"
+      gap="10px">
+      {bought?.map((obj) => (
+        <Box
+          key={obj._id}
+          width="95%"
+          height="auto"
+          display="flex"
+          flexDirection="column"
+          border="0.1px solid lightgrey"
+          borderRadius="5px">
+          <Box
+            display="flex"
+            justifyContent="space-around"
+            sx={{
+              "@media (max-width: 670px)": {
+                flexDirection: "column-reverse",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            }}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              width="45%"
+              height="auto"
+              gap="5px"
+              color="rgba(1,1,1,0.7)"
+              alignSelf="center"
+              sx={{
+                "@media (max-width: 670px)": {
+                  width: "85%",
+                },
+              }}>
+              <Typography
+                width="100%"
+                whiteSpace="nowrap"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                sx={{ cursor: "pointer" }}>
+                {obj.name}
+              </Typography>
+            </Box>
+            <Box
+              width="100px"
+              height="130px"
+              sx={{
+                "@media (max-width: 670px)": {
+                  width: "60%",
+                },
+              }}>
+              <img
+                onClick={() => {
+                  dispatch({
+                    type: "SET_PRODUCTID",
+                    payload: obj._id,
+                  });
+                  navigate("/product");
+                }}
+                width="100%"
+                height="130px"
+                style={{
+                  objectFit: "contain",
+                  border: "1px solid transparent",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+                src={obj.displayImage}
+                alt="image"
+              />
+            </Box>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+}
+
+export default OrderDetails;
