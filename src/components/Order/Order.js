16e1922df757c4f6caeb770fbc27f063b14e76c0
vandana104 <111223104+vandana104@users.noreply.github.com>
2023-10-22 01:21:07 +0530

    Update Order.js

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,25 +4,23 @@ import Modal from "@mui/material/Modal";
 import axios from "axios";
 import React, { useState } from "react";
 import { useStateProvider } from "../../utils/StateProvider";
+import { useNavigate } from "react-router-dom";
 
 function Order() {
-  const [{ productId, totalPrice }, dispatch] = useStateProvider();
+  const [{ productId, totalPrice, token, buyProducts }, dispatch] =
+    useStateProvider();
+  const navigate = useNavigate();
   const [address, setAddress] = useState("");
   const [street, setStreet] = useState("");
   const [city, setCity] = useState("");
   const [states, setStates] = useState("");
   const [country, setCountry] = useState("");
   const [zip, setZip] = useState("");
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   const handleOrder = (e) => {
     e.preventDefault();
-    const Headers = {
-      projectID: "f104bi07c490",
-    };
-    const Body = {
+
+    const body = JSON.stringify({
       productId: productId,
       quantity: 2,
       addressType: "HOME",
@@ -33,118 +31,120 @@ function Order() {
         country: country,
         zipCode: zip,
       },
-    };
+    });
     axios
-      .post(
+      .get(
         "https://academics.newtonschool.co/api/v1/ecommerce/order",
-        Headers,
-        Body,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            projectId: "f104bi07c490",
+          },
+        },
+        body,
       )
       .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        if (err.message === "Network Error") {
+          alert("Please check your connection");
+        }
+      });
+    const bought = JSON.parse(localStorage.getItem("bought")) || [];
+    const filteredItem = [...bought, ...buyProducts];
+    localStorage.setItem("bought", JSON.stringify(filteredItem));
+    alert(
+      `This is you total amount ${totalPrice} Cash on delivery is only available`,
+    );
+    navigate("/orders");
   };
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
-  return (
-    <Box>
-      <Typography
-        textAlign="center"
-        fontSize="26px"
-        fontWeight="600"
-        color="rgba(1,1,1,0.8)">
-        Buy Now
-      </Typography>
-      <Typography color="lightgreen">Total Price : {totalPrice}</Typography>
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "50%",
-          margin: "auto",
-          marginBottom: "40px",
-          gap: "10px",
-        }}>
-        <label for="address">Enter Your Address</label>
-        <TextField
-          sx={{}}
-          required
-          id="address"
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Email"
-        />
-        <label for="street">Enter Your Street</label>
-        <TextField
-          sx={{}}
-          required
-          id="street"
-          onChange={(e) => setStreet(e.target.value)}
-          placeholder="Street"
-        />
-        <label for="city">Enter Your City</label>
-        <TextField
-          sx={{}}
-          required
-          id="city"
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="City"
-        />
-        <label for="state">Enter Your State</label>
-        <TextField
-          sx={{}}
-          required
-          id="state"
-          onChange={(e) => setStates(e.target.value)}
-          placeholder="State"
-        />
-        <label for="country">Enter Your Country</label>
-        <TextField
-          sx={{}}
-          required
-          id="country"
-          onChange={(e) => setCountry(e.target.value)}
-          placeholder="Country"
-        />
-        <label for="zip">Enter Your Zip code</label>
-        <TextField
-          sx={{}}
-          required
-          id="zip"
-          onChange={(e) => setZip(e.target.value)}
-          placeholder="zip"
-        />
-        <Button
-          onClick={handleOpen}
-          variant="contained"
-          type="submit"
-          sx={{
-            mt: "10px",
-            width: "70%",
-            alignSelf: "center",
-            padding: "10px",
-          }}>
-          Buy Now
-        </Button>
-      </form>
 
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Total Price : {totalPrice}
-          </Typography>
-        </Box>
-        <Button onClick={handleOrder}>Confirm Order</Button>
-      </Modal>
-    </Box>
+  return (
+    <>
+      <Box>
+        <Typography
+          textAlign="center"
+          fontSize="26px"
+          fontWeight="600"
+          color="rgba(1,1,1,0.8)">
+          Buy Now
+        </Typography>
+        <marquee direction="right" scrollamount="10" style={{ margin: "10px" }}>
+          <Typography color="lightgreen">Total Price : {totalPrice}</Typography>
+        </marquee>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "50%",
+            margin: "auto",
+            marginBottom: "40px",
+            gap: "10px",
+          }}>
+          <label>Enter Your Address</label>
+          <TextField
+            sx={{}}
+            required
+            id="address"
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Email"
+          />
+          <label>Enter Your Street</label>
+          <TextField
+            sx={{}}
+            required
+            id="street"
+            onChange={(e) => setStreet(e.target.value)}
+            placeholder="Street"
+          />
+          <label>Enter Your City</label>
+          <TextField
+            sx={{}}
+            required
+            id="city"
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="City"
+          />
+          <label>Enter Your State</label>
+          <TextField
+            sx={{}}
+            required
+            id="state"
+            onChange={(e) => setStates(e.target.value)}
+            placeholder="State"
+          />
+          <label>Enter Your Country</label>
+          <TextField
+            sx={{}}
+            required
+            id="country"
+            onChange={(e) => setCountry(e.target.value)}
+            placeholder="Country"
+          />
+          <label>Enter Your Zip code</label>
+          <TextField
+            sx={{}}
+            required
+            id="zip"
+            onChange={(e) => setZip(e.target.value)}
+            placeholder="zip"
+          />
+          <Button
+            onClick={handleOrder}
+            variant="contained"
+            type="submit"
+            sx={{
+              mt: "10px",
+              width: "70%",
+              alignSelf: "center",
+              padding: "10px",
+            }}>
+            Buy Now
+          </Button>
+        </form>
+      </Box>
+    </>
   );
 }
 
